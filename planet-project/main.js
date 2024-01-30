1a7824c271b76f47639e63fde9152772b96b2b3e
Brady Peterson <bpeterson1663@gmail.com>
2024-01-30 12:19:07 -0600

    feat(planet): read kepler csv data

--- planet-project/main.js
+++ planet-project/main.js
@@ -1,1 +1,21 @@
-import { parse } from 'csv-parse';
+const { parse } = require('csv-parse')
+const fs = require('fs')
+
+const results = []
+const parser = parse({
+    comment: '#',
+    columns: true
+})
+
+fs.createReadStream('kepler_data.csv')
+    .pipe(parser)
+    .on('data', (data) => {
+        results.push(data)
+    })
+    .on('error', (err) =>{
+        console.error(err)
+    })
+    .on('end', () => {
+        console.log("end")
+        console.log(results)
+    })
